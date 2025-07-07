f5a798500ae9130f187c894c47a8a7e257b3b50f
JaJo EkiZ <jajoultrapunk@gmail.com>
2025-07-07 02:49:57 -0300

    creacion de controlador API, configuracion de bootstrap/app.php habilitando API y nuevos Endpoints

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Reclamo;
+use App\Http\Controllers\Api\ReclamoApiController;
 
 Route::middleware('auth:sanctum')->get('/reclamos', function (Request $request) {
     return Reclamo::with('tipo.categoria')->where('user_id', $request->user()->id)->get();
 });
+
+Route::get('/reclamos-por-estado', [ReclamoApiController::class, 'reclamosPorEstado']);
+
+Route::get('/reclamos-por-usuario', [ReclamoApiController::class, 'reclamosPorUsuario']);
+
+Route::get('/resumen-usuario', [ReclamoApiController::class, 'resumenPorUsuario']);
+
